a7022c37da141eca06d4ca063592f76d2acc6cac
Cristhiam Martinez <crismartinez@t-online.de>
2024-04-23 12:45:50 +0200

    Functions for creating the feature vector were created. TODO: Create plot functions according to experiments

--- Functions.py
+++ Functions.py
@@ -3,7 +3,6 @@ from scipy.signal import find_peaks
 import numpy as np
 import constants as c
 
-
 class Preprocessing:
     """Loads all functions for data preprocessing"""
     def __init__(self):
@@ -11,17 +10,49 @@ class Preprocessing:
         self.n_fft = 4096
         self.fft_freqs = librosa.fft_frequencies(sr = c.SR, n_fft=self.n_fft) # 4096 gives results that approach the 1/4 Tone resolution
 
-    # load time series
+    # Docstring for load_data function
     def load_data(self, signal):
+        """
+        Loads a time series data from a txt file.
+
+        Args:
+            signal (str): Path to the signal file.
+
+        Returns:
+            np.ndarray: The loaded time series data.
+        """
         data = np.loadtxt(signal)
         return data#[:24000]
 
+    # Docstring for fft_librosa_laplace function
     def fft_librosa_laplace(self, data):
+        """
+        Calculates the Short-Time Fourier Transform (STFT) of a time series signal using librosa library.
+
+        Args:
+            data (np.ndarray): The time series data.
+
+        Returns:
+            np.ndarray: The magnitude of the STFT (complex absolute value).
+        """
         signal_fft = librosa.stft(data, n_fft=self.n_fft, hop_length=96) # 8192 = 46.8 ms
         signal_fft_abs = np.abs(signal_fft)
         return signal_fft_abs
 
+    # Docstring for peak_extraction function
     def peak_extraction(self, signal_fft_abs, time_value, distance, width):
+        """
+        Extracts peaks from the magnitude of the STFT using scipy.signal.find_peaks function.
+
+        Args:
+            signal_fft_abs (np.ndarray): The magnitude of the STFT.
+            time_value (list): Time t at which the peak picking is performed.
+            distance (int): Minimum distance (in samples) between two peaks.
+            width (int): Minimum prominence of a peak.
+
+        Returns:
+            list: List of peak indices in the STFT.
+        """
         # modify the range function according to the time steps aimed at
         if len(time_value) == 1:
             extracted_peaks, _ = find_peaks (signal_fft_abs.T[time_value[0]], distance=distance, width=width)
@@ -29,15 +60,36 @@ class Preprocessing:
         else:
             for time in time_value:
                 extracted_peaks, _ = find_peaks (signal_fft_abs.T[time], distance=distance, width=width)
-                # peaks_per_time.append (extracted_peaks)
         return extracted_peaks
 
+    # Docstring for data_processing_laplace function
     def data_processing_laplace(self, signal):
+        """
+        Performs data processing for Laplace analysis:
+         - Loads data
+         - Calculates STFT magnitude
+
+        Args:
+            signal (str): Path to the signal file.
+
+        Returns:
+            np.ndarray: The magnitude of the STFT.
+        """
         data = self.load_data(signal)
         signal_fft = self.fft_librosa_laplace(data) # using librosa
         return signal_fft
 
+    # Docstring for laplace_extraction function
     def laplace_extraction(self, file):
+        """
+        Extracts Laplace value from the filename based on a defined format.
+
+        Args:
+            file (str): Path to the signal file.
+
+        Returns:
+            list: List containing the extracted Laplace value (float).
+        """
         file = str (file)
         parts = file.split ('_')
         laplace_value = []
@@ -48,7 +100,17 @@ class Preprocessing:
         laplace_value = [int (value.replace (',', '.')) for value in laplace_value]
         return laplace_value
 
-    def damping_extraction(self, file): # only used for plots
+    # Docstring for damping_extraction function
+    def damping_extraction(self, file):
+        """
+        Extracts damping value from the filename based on a defined format (for plots only).
+
+        Args:
+            file (str): Path to the signal file.
+
+        Returns:
+            list: List containing the extracted damping value
+        """
         file = str (file)
         parts = file.split ('_')
         damping_value = []
@@ -59,33 +121,23 @@ class Preprocessing:
         damping_value = [float(value.replace(',', '.')) for value in damping_value]
         return damping_value
 
-    def gammaconst_damping_extraction(self, file): # only used for plots
-        file = str (file)
-        parts = file.split ('_')
-        Damping_value = []
-        if len (parts) >= 9:
-            desired_part = parts[6]#.split ('.')[0]  # Adjust the index based on your file name structure
-        Damping_value.append (desired_part)
-        return Damping_value
-
-
-# def analyze_spectrum(path_to_dataset, ht_length, vdf_frequencies):
-#     laplace_variations_analysis_vector = []
-#     for length in ht_length:
-#         for freq in vdf_frequencies:
-#             for file in sorted (path_to_dataset.glob (f'FrameDrum_{length}_{freq}*.txt')):
-#                 fft_result = data_processing_laplace (file)
-#                 peaks_result = peak_extraction (fft_result,
-#                                                   time_value,
-#                                                   distance=c.DISTANCE,
-#                                                   width=c.WIDTH)
-#                 laplace_variations_analysis_vector.append ({
-#                     "archivo": file.name,
-#                     "n_buff": length,
-#                     "Frequency": freq,
-#                     "LaPlace": laplace_extraction (file),
-#                     "Damping": damping_extraction (file),
-#                     "FFT": fft_result,
-#                     "Peaks": peaks_result
-#                 })
-#     return laplace_variations_analysis_vector
+    def analyze_spectrum(self, path_to_dataset, ht_length, vdf_frequencies):#, self.n_fft):
+        laplace_variations_analysis_vector = []
+        for length in ht_length:
+            for freq in vdf_frequencies:
+                for file in sorted (path_to_dataset.glob (f'FrameDrum_{length}_{freq}*.txt')):
+                    fft_result = self.data_processing_laplace (file)
+                    peaks_result = self.peak_extraction (fft_result,
+                                                         c.TIME_VALUE,
+                                                         distance=c.DISTANCE,
+                                                         width=c.WIDTH)
+                    laplace_variations_analysis_vector.append ({
+                        "archivo": file.name,
+                        "n_buff": length,
+                        "Frequency": freq,
+                        "LaPlace": self.laplace_extraction (file),
+                        "Damping": self.damping_extraction (file),
+                        "FFT": fft_result,
+                        "Peaks": peaks_result
+                    })
+        return laplace_variations_analysis_vector
--- ViscoSOM.py
+++ ViscoSOM.py
@@ -1,6 +1,5 @@
 from Functions import Preprocessing
 from pathlib import Path
-import constants as c
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,79 +7,105 @@ import matplotlib.colors as mcolors
 from matplotlib.gridspec import GridSpec
 from minisom import MiniSom
 
-f = Preprocessing ()
-def analyze_spectrum(path_to_dataset, ht_length, vdf_frequencies, n_fft):
-    laplace_variations_analysis_vector = []
-    for length in ht_length:
-        for freq in vdf_frequencies:
-            for file in sorted (path_to_dataset.glob (f'FrameDrum_{length}_{freq}*.txt')):
-                fft_result = f.data_processing_laplace (signal=file)
-                peaks_result = f.peak_extraction (fft_result,
-                                                  time_value,
-                                                  distance=c.DISTANCE,
-                                                  width=c.WIDTH)
-                laplace_variations_analysis_vector.append ({
-                    "archivo": file.name,
-                    "n_buff": length,
-                    "Frequency": freq,
-                    "LaPlace": f.laplace_extraction (file),
-                    "Damping": f.damping_extraction (file),
-                    "FFT": fft_result,
-                    "Peaks": peaks_result
-                })
-    return laplace_variations_analysis_vector
+def extract_peaks_from_fr(analysis_vector, length_ht, n_buff, VDF, min_freq, max_freq, f):
+    """
+    Extracts peaks within a specific frequency range from Laplace analysis results.
 
-def extract_peaks_from_fr(analysis_vector):
-    # Create an empty list to store dictionaries for DataFrame
+    Args:
+        analysis_vector (list): List of dictionaries containing Laplace analysis results for each signal file.
+        length_ht (bool): if True, only the time integration defined by n_buff will be analyzed. If False, all T are analyzed.
+        n_buff (int): Target integration time to consider (if length_ht is True). 384 = 4ms, 672 = 7ms, 960 = 10ms.
+        VDF (int): Viscoelatically Damped Frequency.
+        min_freq (float): Lower bound of the frequency range to extract peaks from.
+        max_freq (float): Upper bound of the frequency range to extract peaks from.
+        f (Preprocessing object): An instance of the Preprocessing class.
+
+    Returns:
+        list: List of peak frequencies within the specified range for each analysis result.
+    """
     peaks_in_freq_range = []
-    # Iterate over laplace_variations_analysis_vector
     for laplace_result in analysis_vector:
-        # if laplace_result['n_buff'] == n_buff and laplace_result['Frequency'] == VDF:
-        if laplace_result['Frequency'] == VDF:
+        # Check for VDF and handle length_ht condition in one step
+        if laplace_result['Frequency'] == VDF and (length_ht is False or laplace_result['n_buff'] == n_buff):
             for peaks in laplace_result["Peaks"]:
                 if min_freq < f.fft_freqs[peaks] < max_freq:
-                    peaks_in_freq_range.append (f.fft_freqs[peaks])
+                    peaks_in_freq_range.append(f.fft_freqs[peaks])
     return peaks_in_freq_range
 
-def extract_peaks_info(sorted_peaks):
-    # [0] == peak frequencies, [1] == peak indices
+
+def extract_peaks_info(sorted_peaks, f):
+    """
+    Extracts information about peaks from a sorted list of frequencies.
+
+    Args:
+        sorted_peaks (list): A list of frequencies in ascending order.
+        f (Preprocessing object): An instance of the Preprocessing class.
+
+    Returns:
+        list: A list containing two sub-lists:
+              - The first sub-list contains the extracted peak frequencies.
+              - The second sub-list contains the corresponding indices in f.fft_freqs.
+    """
     peaks_info = [[], []]
     for peaks in sorted_peaks:
-        for idx, hz in enumerate (list (f.fft_freqs)):
+        for idx, hz in enumerate(list(f.fft_freqs)):
             if peaks == hz:
-                # print(peaks, hz, idx)
-                peaks_info[0].append (hz)
-                peaks_info[1].append (list (f.fft_freqs).index (peaks))
+                peaks_info[0].append(hz)
+                peaks_info[1].append(list(f.fft_freqs).index(peaks))
     return peaks_info
 
-def create_feature_vector(analysis_vector):
-    # global feature_vector_dict
 
-    # create a key for each frequency inside the frequency range
+def create_feature_vector(analysis_vector, peaks_info, time_value, n_buff):
+    """
+    Creates a feature vector dictionary from Laplace analysis results.
+
+    Args:
+        analysis_vector (list): List of dictionaries containing Laplace analysis results for each signal file.
+        peaks_info (list): List containing peak frequencies and their corresponding indices in f.fft_freqs.
+        time_value (int): Time t at which the peak picking is performed.
+        n_buff (int): Target integration time to consider (if length_ht is True). 384 = 4ms, 672 = 7ms, 960 = 10ms.
+
+    Returns:
+        dict: A dictionary containing features for each analysis result.
+    """
+    # Define keys based on frequency values in the peaks_info list
     feature_vector_dict = {value: [] for value in peaks_info[0]}
 
-    # add the keys "Damping" and "LaPlace"
-    feature_vector_dict["Damping"] = []
-    feature_vector_dict["LaPlace"] = []
-    feature_vector_dict["buff"] = []
+    # Add extra keys for Damping, LaPlace, and buff
+    som_metadata = ["Damping", "LaPlace", "buff"]
+    feature_vector_dict.update({key: [] for key in som_metadata})
 
-    # extract and append values to the dictionary
-    for idx, laplace_result in enumerate (analysis_vector):
-        # TODO: create the case for a specific n_buff
-        # if laplace_result['n_buff'] == n_buff and laplace_result['Frequency'] == VDF:
-        if laplace_result['Frequency'] == VDF:
-            feature_vector_dict['Damping'].append (laplace_result['Damping'][0])
-            feature_vector_dict['LaPlace'].append (laplace_result['LaPlace'][0])
-            feature_vector_dict['buff'].append (laplace_result['n_buff'])
+    # Extract and append values to the dictionary
+    for idx, laplace_result in enumerate(analysis_vector):
+        # Check for n_buff and VDF
+        if laplace_result['n_buff'] == n_buff and laplace_result['Frequency'] == VDF:
+            feature_vector_dict['Damping'].append(laplace_result['Damping'][0])
+            feature_vector_dict['LaPlace'].append(laplace_result['LaPlace'][0])
+            feature_vector_dict['buff'].append(laplace_result['n_buff'])
 
-            for peak_freqs, peak_index in zip (peaks_info[0], peaks_info[1]):
+            for peak_freqs, peak_index in zip(peaks_info[0], peaks_info[1]):
                 key_to_append = peak_freqs
-
                 if key_to_append in feature_vector_dict:
-                    feature_vector_dict[key_to_append].append (laplace_result["FFT"][peak_index][time_value][0])
+                    feature_vector_dict[key_to_append].append(laplace_result["FFT"][peak_index][time_value][0])
     return feature_vector_dict
 
+def create_vector_for_som():
+    global feature_vector_dataframe
+    # FEATURE VECTOR
+    peaks_in_freq_range = extract_peaks_from_fr (laplace_variations_analysis_vector, length_ht=False, n_buff=n_buff,
+                                                 VDF=VDF, min_freq=min_freq, max_freq=max_freq, f=f)
+    # erasing duplicated peaks and sorting numerically
+    sorted_peaks = list (sorted (dict.fromkeys (peaks_in_freq_range)))
+    # extract freq. and amp. from each peak
+    peaks_info = extract_peaks_info (sorted_peaks, f)
+    # create the dictionary for the feature vector
+    feature_vector_dict = create_feature_vector (laplace_variations_analysis_vector, peaks_info,
+                                                 time_value=time_value, n_buff=n_buff)
+    # Create a DataFrame from the feature vector dict
+    feature_vector_dataframe = pd.DataFrame (feature_vector_dict)
 
+    return feature_vector_dataframe
+f = Preprocessing ()
 
 # Parameters of the simulation
 ht_length = [384, 672, 960]
@@ -88,38 +113,24 @@ vdf_frequencies = [700, 2000, 3300, 6400, 12000]
 directory_path_laplace = Path (r'C:\Users\LEGION\PycharmProjects\pythonProject\venv\Laplace variations')
 directory_path_gammaconst = Path (r'C:\Users\LEGION\PycharmProjects\pythonProject\venv\gammaconst')
 
+"""Run STFT and Peak Extraction"""
+laplace_variations_analysis_vector = f.analyze_spectrum (path_to_dataset=directory_path_laplace,
+                                                       ht_length=ht_length,
+                                                       vdf_frequencies=vdf_frequencies)
+
 # Parameters of the pipeline
 
 time_value = [60]  # time t at which peak picking is performed
-# n_buff = 672
+n_buff = 960 # integration time T
 VDF = 12000
-frequency_range = 1000 / 2
-n_fft = 4096
+frequency_range = 1000 / 2 # FR of the analysis Freq. Range
+n_fft = 4096 # Window size of the n_fft
 
 # Specify the frequency range
 min_freq = VDF - frequency_range
 max_freq = VDF + frequency_range
 
-"""Run STFT and Peak Extraction"""
-laplace_variations_analysis_vector = analyze_spectrum (path_to_dataset=directory_path_laplace,
-                                                       ht_length=ht_length,
-                                                       vdf_frequencies=vdf_frequencies,
-                                                       n_fft=n_fft)
-
-# FEATURE VECTOR
-peaks_in_freq_range = extract_peaks_from_fr (laplace_variations_analysis_vector)
-
-# erasing duplicated peaks and sorting numerically
-sorted_peaks = list(sorted(dict.fromkeys(peaks_in_freq_range)))
-
-# extract freq. and amp. from each peak
-peaks_info = extract_peaks_info (sorted_peaks)
-
-# create the dictionary for the feature vector
-feature_vector_dict = create_feature_vector (laplace_variations_analysis_vector)
-
-# Create a DataFrame from the feature vector dict
-feature_vector_dataframe = pd.DataFrame(feature_vector_dict)
+feature_vector_dataframe = create_vector_for_som ()
 
 # The last three columns correspond to metadata for map calibration
 data = feature_vector_dataframe[feature_vector_dataframe.columns[:-3]]
@@ -173,23 +184,13 @@ colors[0] = [0.5, 0.5, 0.5, 1.0]  # Gray color for the 0 value
 # Create a new colormap with the modified colors
 cmap_modified = mcolors.ListedColormap(colors)
 
+
 plt.figure(figsize=(10, 10))
 plt.pcolor(som.distance_map().T, cmap=cmapcolor, alpha=alpha)
-# Eliminate white space around plot elements
-
-# without legend
-# plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95)
-
-# with legend
-# plt.subplots_adjust(left=0.05, right=0.95, bottom=0.1, top=0.95)
-# plt.colorbar()
-
 unique_values = np.unique(target1)
 
 for idx, c in enumerate(unique_values):
     idx_target = target1 == c
-
-    # Choose a color from the Viridis colormap
     color = cmap_modified (idx / len (unique_values))
 
     # Define size based on target2 values (scale and normalize)
@@ -199,44 +200,177 @@ for idx, c in enumerate(unique_values):
                 w_y[idx_target] + .5 + (np.random.rand(np.sum(idx_target)) - .5) * .8,
                 s=size_values, marker='o', color=color, label=f'{c}', alpha=1, edgecolors='black', linewidths=1)
 
-# plt.title(f'VDF: 700Hz - FR: {min_freq}Hz - {max_freq}Hz - {time_value[0]}ms')
-# plt.grid()
-plt.yticks(range(0, n_neurons+1, 5), fontsize = 'x-large')
-plt.xticks(range(0, n_neurons+1, 5), fontsize = 'x-large')
-
-# plt.yticks([])
-# plt.xticks ([])
-
-# plt.legend(loc='upper center', bbox_to_anchor=(0.55, -0.03), ncol=6, fontsize = 'large')
+plt.yticks([])
+plt.xticks ([])
 plt.tight_layout()
-plt.savefig('SOM LaPlace and Damping.png', dpi=300)
+# plt.savefig('SOM LaPlace and Damping.png', dpi=600)
 plt.show()
 plt.close()
+
+
+
+#%%
+# def plot_som(length_ht, legend, title, ticks, save, umatrix_colorbar, markers):
+#     # global c
+#
+#     if legend:
+#         figsize = (10, 11)
+#         bottom_adjust = 0.1
+#     else:
+#         figsize = (10, 10)
+#         bottom_adjust = 0.05
+#
+#     plt.figure (figsize=figsize)
+#     plt.subplots_adjust (left=0.05, right=0.95, bottom=bottom_adjust, top=0.95)
+#
+#     plt.pcolor (som.distance_map ().T, cmap=cmapcolor, alpha=alpha)
+#
+#     if umatrix_colorbar:
+#         plt.colorbar()
+#
+#     if length_ht:
+#         unique_values = np.unique (target3)
+#         colors = ['red', 'blue', 'yellow']
+#
+#         for idx, c in enumerate (unique_values):
+#             idx_target = target3 == c
+#
+#             if c == 384:
+#                 # marker = markers[0]
+#                 label = f'4ms'
+#             elif c == 672:
+#                 # marker = markers[1]
+#                 label = f'7ms'
+#             elif c == 960:
+#                 # marker = markers[2]
+#                 label = f'10ms'
+#             else:
+#                 marker = 'o'  # Default marker if target3 doesn't match any condition
+#
+#             color = colors[idx]
+#
+#     else:
+#         # color according to Re
+#         unique_values = np.unique (target1)
+#         for idx, c in enumerate (unique_values):
+#             idx_target = target1 == c
+#             color = cmap_modified (idx / len (unique_values))
+#             label = f'{c}'
+#             marker = 'o'
+#
+#     size_values = (target2[idx_target] - np.min (target2[idx_target])) / \
+#                   (np.max (target2[idx_target]) - np.min (target2[idx_target])) * 300  # Adjust max_size as needed
+#
+#     plt.scatter(w_x[idx_target] + .5 + (np.random.rand(np.sum(idx_target)) - .5) * .5,
+#                 w_y[idx_target] + .5 + (np.random.rand(np.sum(idx_target)) - .5) * .8,
+#                 s=size_values, marker='o', color=color, label=f'{c}', alpha=1, edgecolors='black', linewidths=1)
+#
+#     # # Define size based on target2 values (scale and normalize)
+#     # size_values = (target2[idx_target] - np.min (target2[idx_target])) / \
+#     #               (np.max (target2[idx_target]) - np.min (target2[idx_target])) * 300  # Adjust max_size
+#     #
+#     # plt.scatter (w_x[idx_target] + .5 + (np.random.rand (np.sum (idx_target)) - .5) * .5,
+#     #              w_y[idx_target] + .5 + (np.random.rand (np.sum (idx_target)) - .5) * .8,
+#     #              s=size_values, marker=marker, color=color, label=label, alpha=1, edgecolors='black', linewidths=1)
+#
+#     if legend:
+#         plt.legend (loc='upper center', bbox_to_anchor=(0.55, -0.03), ncol=6, fontsize='xx-large')
+#
+#     if legend and length_ht:
+#         plt.legend (loc='upper center', bbox_to_anchor=(0.55, -0.03), ncol=3, fontsize='xx-large')
+#
+#     if legend and length_ht and markers:
+#         handles, labels = plt.gca ().get_legend_handles_labels ()
+#         by_label = dict (zip (labels, handles))
+#         plt.legend (by_label.values (), by_label.keys (), loc='upper center', bbox_to_anchor=(0.55, -0.03), ncol=3,
+#                     fontsize='xx-large')
+#
+#     if title:
+#         plt.title(f'{VDF}Hz - FR: {min_freq}Hz - {max_freq}Hz - t = {time_value[0]}ms')
+#
+#     if title and length_ht:
+#         plt.title (f'{VDF}Hz - FR: {min_freq}Hz - {max_freq}Hz - t = {time_value[0]}ms - INTEGRATION TIME ANALYSIS')
+#     # plt.grid()
+#
+#     if ticks:
+#         plt.yticks (range (0, n_neurons + 1, 5), fontsize='x-large')
+#         plt.xticks (range (0, n_neurons + 1, 5), fontsize='x-large')
+#
+#     else:
+#         plt.yticks([])
+#         plt.xticks([])
+#
+#     plt.tight_layout ()
+#     if save:
+#         plt.savefig ('SOM Re and Gamma.png', dpi=300)
+#     plt.show ()
+#     plt.close ()
+#
+# plot_som (length_ht=False, legend=True, title=True, ticks=True, save=False, umatrix_colorbar=False, markers=False)
+
+# def plot_som(legend):
+#     # global c
+#
+#     if legend:
+#         figsize = (10, 11)
+#         bottom_adjust = 0.1
+#     else:
+#         figsize = (10, 10)
+#         bottom_adjust = 0.05
+#
+#     plt.figure (figsize=figsize)
+#     plt.subplots_adjust (left=0.05, right=0.95, bottom=bottom_adjust, top=0.95)
+#
+#     plt.pcolor (som.distance_map ().T, cmap=cmapcolor, alpha=alpha)
+#
+#     unique_values = np.unique(target1)
+#
+#     for idx, c in enumerate(unique_values):
+#         idx_target = target1 == c
+#
+#         color = cmap_modified (idx / len (unique_values))
+#
+#         # Define size based on target2 values (scale and normalize)
+#         size_values = (target2[idx_target] - np.min (target2[idx_target])) / \
+#                       (np.max (target2[idx_target]) - np.min (target2[idx_target])) * 300  # Adjust max_size as needed
+#         plt.scatter(w_x[idx_target] + .5 + (np.random.rand(np.sum(idx_target)) - .5) * .5,
+#                     w_y[idx_target] + .5 + (np.random.rand(np.sum(idx_target)) - .5) * .8,
+#                     s=size_values, marker='o', color=color, label=f'{c}', alpha=1, edgecolors='black', linewidths=1)
+#     if legend:
+#         plt.legend (loc='upper center', bbox_to_anchor=(0.55, -0.03), ncol=6, fontsize='large')
+#
+#     plt.tight_layout()
+#     # plt.savefig('SOM LaPlace and Damping.png', dpi=600)
+#     plt.show()
+#     plt.close()
+#
+#
+# plot_som(legend=True)
 
 # Plot the contour of the spectrum of the feature vector
 
-win_map = som.win_map(data)
-
-plt.figure(figsize=(10, 10))
-# plt.pcolor(som.distance_map().T, cmap=cmapcolor, alpha=0.5)
-# plt.subplots_adjust(left=0.05, right=0.98, bottom=0.03, top=0.99, wspace=0.6, hspace=0.3)
-plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95)
-plt.yticks(range(0, n_neurons+1, 5), fontsize = 'x-large')
-plt.xticks(range(0, n_neurons+1, 5), fontsize = 'x-large')
-
-the_grid = GridSpec(m_neurons, n_neurons)
-for position in win_map.keys():
-    row, col = m_neurons-1-position[1], position[0]
-    ax = plt.subplot(the_grid[row, col])
-
-    # plt.plot(np.min(win_map[position], axis=0), color='gray', alpha=.5)
-    # plt.plot(np.max(win_map[position], axis=0), color='gray', alpha=.5)
-    plt.plot (np.mean (win_map[position], axis=0), linewidth=1)
-    # plt.yticks (fontsize='small')
-    plt.yticks([])
-    plt.xticks ([])
-
-plt.tight_layout()
-plt.savefig('time series.png', dpi=600)
-plt.show()
-plt.close()
+# win_map = som.win_map(data)
+#
+# plt.figure(figsize=(10, 10))
+# # plt.pcolor(som.distance_map().T, cmap=cmapcolor, alpha=0.5)
+# # plt.subplots_adjust(left=0.05, right=0.98, bottom=0.03, top=0.99, wspace=0.6, hspace=0.3)
+# plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95)
+# plt.yticks(range(0, n_neurons+1, 5), fontsize = 'x-large')
+# plt.xticks(range(0, n_neurons+1, 5), fontsize = 'x-large')
+#
+# the_grid = GridSpec(m_neurons, n_neurons)
+# for position in win_map.keys():
+#     row, col = m_neurons-1-position[1], position[0]
+#     ax = plt.subplot(the_grid[row, col])
+#
+#     # plt.plot(np.min(win_map[position], axis=0), color='gray', alpha=.5)
+#     # plt.plot(np.max(win_map[position], axis=0), color='gray', alpha=.5)
+#     plt.plot (np.mean (win_map[position], axis=0), linewidth=1)
+#     # plt.yticks (fontsize='small')
+#     plt.yticks([])
+#     plt.xticks ([])
+#
+# plt.tight_layout()
+# plt.savefig('time series.png', dpi=600)
+# plt.show()
+# plt.close()
